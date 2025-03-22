f746c1b68af8899a560b8aaaf27182a33718194f
Sumit <sumitmmishra055@gmail.com>
2025-03-22 17:32:11 +0530

    final code

--- server.js
+++ server.js
@@ -20,16 +20,16 @@ const app=express()
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,'./clients/build')))
+// app.use(express.static(path.join(__dirname,'./clients/build')))
 
 //routes
 app.use("/api/v1/auth",authRoutes);
 app.use('/api/v1/category',categoryRoutes);
 app.use('/api/v1/product',productRoutes)
 //rest api
-app.use('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./clients/build/index.html'));
-})
+// app.use('*',function(req,res){
+//     res.sendFile(path.join(__dirname,'./clients/build/index.html'));
+// })
 
 //port
 const PORT= process.env.PORT ||8080;
